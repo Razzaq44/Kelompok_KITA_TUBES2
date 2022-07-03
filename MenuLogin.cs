c63b91df34fb94900a59c3722a549d84d8759160
Irkhamna Rahma Dewi <rahmadewi@student.telkomuniversity.ac.id>
2022-07-03 21:12:30 +0700

    menambah mekanisme login

--- MenuLogin.cs
+++ MenuLogin.cs
@@ -29,7 +29,47 @@ namespace Kelompok_6_TUBES
 
         private void button1_Click(object sender, EventArgs e)
         {
+            konek.Open();
+            if (textBox1.Text == "" & textBox2.Text == "" & textBox3.Text == "" & !Regex.IsMatch(textBox3.Text, @"^[a-zA-Z][\w.-][a-zA-Z0-9]@[a-zA-Z0-9][\w.-][a-zA-Z0-9].[a-zA-Z][a-zA-Z.]*[a-zA-Z]$"))//Parshing METHOD
+            {
+                MessageBox.Show("Username atau password masih kosong");//Programing Defensive METHOD
+            }
+            else if (textBox2.Text != "Password")
+            {
+                MessageBox.Show("Password tidak sama");
+            }
+            else
+            {
+                SqlDataAdapter adr = new SqlDataAdapter("select Username,Password,Gmail from E-Wallet where Username = '" + textBox1.Text + "' OR Gmail = '" + textBox3.Text + "' AND Password = '" + textBox2.Text + "' ", konek);
+                DataTable dt = new DataTable();
+                adr.Fill(dt);
 
+                //Table Driven METHOD 
+                if (dt.Rows.Count > 0)
+                {
+                    foreach (DataRow dr in dt.Rows)
+                    {
+                        if (dr["Pengguna"].ToString() == "admin")
+                        {
+                            this.Hide();
+                            MenuAdmin MA = new MenuAdmin();
+                            MA.Show();
+                        }
+                        else if (dr["Pengguna"].ToString() == "mahasiswa")
+                        {
+                            this.Hide();
+                            MenuUtama MU = new MenuUtama();
+                            MU.Show();
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("username atau password salah", "Warning !!!", MessageBoxButtons.OK, MessageBoxIcon.Error);//Programing Defensive METHOD
+                }
+                //Table Driven METHOD
+            }
+            konek.Close();
         }
     }
 }
