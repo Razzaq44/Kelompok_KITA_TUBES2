9258244f50491a087c9620a0806bf4baa0fac6c2
Irkhamna Rahma Dewi <rahmadewi@student.telkomuniversity.ac.id>
2022-07-03 23:19:09 +0700

    Commit Fix

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Kelompok_6_TUBES
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MenuLogin());
         }
     }
 }
